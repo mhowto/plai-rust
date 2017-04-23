41bc2c8558461c6ec73a096a4e72868513d44d46
mhowto <mhowto@gmail.com>
2017-04-23 15:16:20 +0800

    plus done

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -2,16 +2,19 @@ use ty::Expression;
 
 type Location = u64;
 
+#[derive(Debug,PartialEq)]
 pub struct Binding {
     name: String,
     val: Location,
 }
 
+#[derive(Debug,PartialEq)]
 pub enum Env {
     mt_env,
     extend_env{with: Binding, rest: Box<Env>}
 }
 
+#[derive(Debug,PartialEq)]
 pub enum Value {
     NilV,
     NumV(isize),
@@ -47,12 +50,29 @@ pub struct IResult {
     pub sto: Store,
 }
 
+fn num_op(left: &Value, right: &Value) -> Option<Value> {
+    match (left, right) {
+        (&Value::NumV(l), &Value::NumV(r)) => Option::Some(Value::NumV(l + r)),
+        _ => Option::None
+    }
+
+}
+
 fn interp(expr: &Expression, env: &mut Env, sto: &mut Store) -> Value {
-    match *expr {
-        Expression::Nil     => Value::NilV,
-        Expression::True    => Value::BoolV(true),
-        Expression::False   => Value::BoolV(false),
-        Expression::Num(n)  => Value::NumV(n),
+    match expr {
+        &Expression::Nil     => Value::NilV,
+        &Expression::True    => Value::BoolV(true),
+        &Expression::False   => Value::BoolV(false),
+        &Expression::Num(n)  => Value::NumV(n),
+        &Expression::Plus(ref left, ref right) =>  {
+            let r1 = interp(left.as_ref(), env, sto);
+            let r2 = interp(right.as_ref(), env, sto);
+            if let Option::Some(v) = num_op(&r1, &r2) {
+                v
+            } else {
+                panic!("type error");
+            }
+        }
         _                   => Value::NilV,
     }
 }
